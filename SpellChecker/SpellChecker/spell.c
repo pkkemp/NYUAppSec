53b989c6f82352f6b5e65edff61ad017272deb9a
Preston Kemp <preston@prestonkemp.com>
2019-09-27 10:25:24 -0500

    test

--- SpellChecker/SpellChecker/spell.c
+++ SpellChecker/SpellChecker/spell.c
@@ -19,7 +19,6 @@ int check_words(FILE* fp, hashmap_t hashtable[], char* misspelled[]);
 bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]);
 bool check_word(const char* word, hashmap_t hashtable[]);
 
-//test comment
 
 void lower_string(char s[]) {
    int c = 0;
